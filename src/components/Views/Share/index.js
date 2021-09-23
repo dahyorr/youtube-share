11773abafc49fedd9c907230ed45849e25e021dd
Dayo Adebanjo <dahyor@outlook.com>
2021-09-23 23:55:17 +0100

    Completed App Functionality

--- src/components/Views/Share/index.js
+++ src/components/Views/Share/index.js
@@ -2,12 +2,31 @@ import React, {useState} from 'react'
 import './Share.scss'
 import SearchForm from '../../forms/SearchForm'
 import VideoItem from '../../VideoItem'
+import Loader from '../../Loader'
 import YoutubeApi from '../../../helpers/YoutubeApi'
+import {useAuth, useDb} from '../../../hooks'
+import {toast} from 'react-toastify'
 
-
-const Share = () => {
+const Share = ({history}) => {
     const [videos, setVideos] = useState([])
-    const [loading, setLoading] = useState([])
+    const [loading, setLoading] = useState(false)
+    const {user} = useAuth()
+    const {addNewShare} = useDb()
+
+    const onShare = async (video) => {
+        const data = {...video, author: user.email, date: new Date().getTime()}
+        try{
+            setLoading(true)
+            await addNewShare(data)
+            setLoading(false)
+            toast.success("Video Shared Successfully")
+            history.push('/')
+        }
+        catch(err){
+            toast.error("An error occured")
+        }
+
+    }
 
     const onSearch = (term) => {
         YoutubeApi.get('/search', {
@@ -24,17 +43,18 @@ const Share = () => {
         })
     }
 
-    const renderedVideos = videos.map(video => <VideoItem video={video} key={video.id.videoId}/>)
+    const renderedVideos = videos.map(video => <VideoItem video={video} key={video.id.videoId} onShare={onShare}/>)
 
     return (
+        <>
         <div className="Share">
-            <div className="search-box">
-                <SearchForm onSearch={onSearch} />
-                <div className="result-list">
-                    {renderedVideos}
-                </div>
+            <SearchForm onSearch={onSearch} />
+            <div className="result-list">
+                {renderedVideos}
             </div>
         </div>
+        {loading && <Loader/>}
+        </>
     )
 }
 
