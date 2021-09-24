0be30337a76b608d2df516140a9c8f84977a071d
Dayo Adebanjo <dahyor@outlook.com>
2021-09-24 16:09:27 +0100

    fixed SearchForm.js input Blur

--- src/components/forms/SearchForm.js
+++ src/components/forms/SearchForm.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Input } from 'antd'
 import './Form.scss'
 
 const SearchForm = ({onSearch}) => {
+    const inputRef = useRef()
+
     const handleSearch = (value) => {
+        inputRef.current.blur()
         const term = value.trim()
         if(term.length > 0) {
             onSearch(term);
@@ -16,6 +19,7 @@ const SearchForm = ({onSearch}) => {
                 placeholder="Search for a video"
                 enterButton="Search"
                 size="large"
+                ref={inputRef}
                 onSearch={handleSearch}
             />
         </div>
